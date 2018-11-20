e356613c554b556348028b409af8fe339b0057c9
Rebecca Stone <ysbecca@gmail.com>
2018-11-20 16:23:19 +0000

    update jupyter notebook on using py-wsi

--- py_wsi/config.py
+++ py_wsi/config.py
@@ -8,19 +8,3 @@ Author @ysbecca
 
 
 STORAGE_TYPES 			= ['lmdb', 'hdf5', 'disk']
-
-
-
-
-
-"""
-UPDATE NOTES
-================
-
-lmdb is default so as to be backwards compatible.
-database name not required, but used as prefix in non-lmdb cases.
-
-Requires PIL >= 5.0.0
-h5py
-
-"""
